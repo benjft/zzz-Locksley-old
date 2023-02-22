617844126ec97761030a92ec28e8af0e994eb15d
Benedict Thompson <benjfthompson@gmail.com>
2023-02-22 23:19:12 +0000

    Created logger for logcat

---
+++ Locksley/Platforms/Android/Services/AndroidLoggingProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Locksley.Platforms.Android.Services; 
+
+public class AndroidLoggingProvider : ILoggerProvider {
+    public void Dispose() { }
+
+    public ILogger CreateLogger(string categoryName) {
+        categoryName = categoryName.Split('.').Last();
+        return new AndroidLogger(categoryName);
+    }
+}
